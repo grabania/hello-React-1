66f2c73f9c0e2c747a21fe73f9ef85203f14d806
grabania <grabania.work@gmail.com>
2020-12-02 17:45:45 +0000

    Add functional component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+// ReactDOM.render(
+//   <div>
+//     <h1>Hello React</h1>
+//     <p>This is a paragraph</p>
+//   </div>,
+//   document.getElementById("root")
+// )
+
 import React from "react"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById("root"))
+function MyApp() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}
+
+ReactDOM.render(<MyApp />, document.getElementById("root"))
